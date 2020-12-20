d46238f88a72230a3d89bff80a5b18ce2825e7f9
Ryan Harris <harrisryan1@gmail.com>
2020-12-20 18:52:49 -0500

    add form validation to JoinForm

--- src/client/components/join/JoinForm.tsx
+++ src/client/components/join/JoinForm.tsx
@@ -1,6 +1,7 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { createUseStyles } from 'react-jss'
 import { toast } from 'react-toastify'
+import { useForm } from 'react-hook-form'
 
 import { NotificationPermisionStatus } from '../App'
 
@@ -20,50 +21,74 @@ function usernameIsValid(str: string): boolean {
 function JoinForm(props: JoinFormProps): JSX.Element {
   const { notificationPermission, setUsername } = props
   const styles = useStyles()
-  const [name, setName] = useState<string>('')
+
+  const { register, handleSubmit, watch, errors } = useForm()
+  const name = watch('username', '')
   const validUserName = usernameIsValid(name)
+  console.log('errors', errors)
+
+  const onSubmit = () => {
+    if (validUserName) {
+      setUsername(name)
+
+      if (notificationPermission !== 'granted') {
+        toast.success(`Logged in as ${name}`, {
+          autoClose: 3000,
+        })
+      } else {
+        new Notification(`Logged in as ${name}`)
+      }
+    } else {
+      if (notificationPermission !== 'granted') {
+        toast.error(
+          'Usernames must be between 3 and 20 alphanumeric characters'
+        )
+      } else {
+        new Notification(
+          'Usernames must be between 3 and 20 alphanumeric characters'
+        )
+      }
+    }
+  }
 
   return (
     <div className={styles.joinForm}>
       <h1 className={styles.joinFormHeader}>Welcome to Coder Chat</h1>
-      <form className={styles.joinFormForm}>
+      <form className={styles.joinFormForm} onSubmit={handleSubmit(onSubmit)}>
         <input
           autoFocus
+          name="username"
+          ref={register({
+            minLength: 3,
+            maxLength: 20,
+            pattern: /[a-zA-Z0-9]/,
+            required: true,
+          })}
           className={styles.joinFormInput}
-          required
           placeholder="Enter your name"
           type="text"
-          onChange={e => setName(e.currentTarget.value)}
         />
-        <button
-          className={styles.joinFormButton}
-          type="submit"
-          onClick={e => {
-            e.preventDefault()
-
-            if (validUserName) {
-              setUsername(name)
-
-              if (notificationPermission !== 'granted') {
-                toast.success(`Logged in as ${name}`, {
-                  autoClose: 3000,
-                })
-              } else {
-                new Notification(`Logged in as ${name}`)
-              }
-            } else {
-              if (notificationPermission !== 'granted') {
-                toast.error(
-                  'Usernames must be between 3 and 20 alphanumeric characters'
-                )
-              } else {
-                new Notification(
-                  'Usernames must be between 3 and 20 alphanumeric characters'
-                )
-              }
-            }
-          }}
-        >
+        {errors.username && errors.username.type === 'minLength' && (
+          <span className={styles.joinFormValidationError}>
+            Usernames must have at least 3 characters
+          </span>
+        )}
+        {errors.username && errors.username.type === 'maxLength' && (
+          <span className={styles.joinFormValidationError}>
+            Usernames cannot be longer than 20 charaacters
+          </span>
+        )}
+        {errors.username && errors.username.type === 'required' && (
+          <span className={styles.joinFormValidationError}>
+            Usernames are required
+          </span>
+        )}
+        {errors.username && errors.username.type === 'pattern' && (
+          <span className={styles.joinFormValidationError}>
+            Only alphanumeric characters are allowed
+          </span>
+        )}
+        <button className={styles.joinFormButton} type="submit">
           Join
         </button>
       </form>
@@ -109,5 +134,11 @@ const useStyles = createUseStyles({
     fontSize: '1.2rem',
     fontWeight: 'bold',
     padding: '8px 4px',
+  },
+  joinFormValidationError: {
+    color: 'red',
+    fontSize: '0.75rem',
+    margin: '0 0 8px 0',
+    textAlign: 'left',
   },
 })
